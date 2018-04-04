9e91fd8ed37b32501676afaf403e5de950cf3faf
thejaelal <jay.lal@somaiya.edu>
2018-04-04 12:05:04 +0530

    Added vocabsize to config

--- src/config.py
+++ src/config.py
@@ -1,3 +1,11 @@
+#Cuz the file is inside 'code' directory
+mount_point = "../"
+
+with shelve.open(mount_point+'IAM_Data') as shelf:
+    vocabulary = shelf['chars']
+    
+vocab_size = len(vocabulary)
+
 #Model parameters
 img_height = 104
 img_width = 688
