bug fixes and scaling between -1 to 1 added in model itself

src/Augment.py
@@ -139,9 +139,9 @@ def augment(image):
     #Remove last 2 channels
     image = image[:,:,0]
     
-    ##Convert image to int b/w 0 to 255
+    ##Convert image to int b/w 0 to 255 
     image = image.astype(np.uint8)
-    print("image.type =",image.dtype)
+    #print("image.type =",image.dtype)
     
     #print("After removing last 2 channels",image.shape)
     

src/Image_Fetcher.py
@@ -14,7 +14,7 @@ def fetch(input_dir):
 
 	image_list.sort()
 
-	images = [ cv2.imread(os.path.join(input_dir,image_name),cv2.IMREAD_GRAYSCALE).astype(np.float32)/255.0  for image_name in image_list]
+	images = [ cv2.imread(os.path.join(input_dir,image_name),cv2.IMREAD_GRAYSCALE) for image_name in image_list]
 	# for i,image_name in enumerate(image_list):
 	# 	cv2.imwrite(image_name,images[i])
 

src/model.py
@@ -31,12 +31,18 @@ def ANN_Model():
 
         #Input 'Image'
         inputs = tf.placeholder(tf.float32,shape=[None,img_height,img_width,1])
-
-
+        
+        #Normalize the input Images...
+        inputs = inputs - 128
+        inputs = inputs / 128.0
+        
+        
         #tf.summary.image('images',inputs)
 
+        #Necessary for it to converge, it requires input between 0 to 1.....
         X = inputs
-
+    
+        
         #-------------------Convolution-----------------------#
 
 #         conv = [None] * len(CNN)

src/train.py
@@ -79,7 +79,7 @@ with tf.Session(graph = graph) as sess:
 #             print(y.shape)
 #             print(y[0])
             
-            print("shape of y",y.shape)
+            #print("shape of y",y.shape)
         
             #widths = np.squeeze(widths,axis=1)
             #y = np.squeeze(y,axis=-1)
@@ -162,7 +162,7 @@ with tf.Session(graph = graph) as sess:
             end_time = time.time()
             time_taken = end_time - start_time
             
-            print("Epoch: {}, train_loss:{:.2f}, valid_loss:{:.2f}, ler:{:.2f} in {:.2f} sec.".format(e,train_loss,valid_loss,ler,time_taken)) 
+            #print("Epoch: {}, train_loss:{:.2f}, valid_loss:{:.2f}, ler:{:.2f} in {:.2f} sec.".format(e,train_loss,valid_loss,ler,time_taken)) 
             
             #Save the model
             saver.save(sess,mount_point+'saved_models/cnn_lstm_fc_'+str(e))